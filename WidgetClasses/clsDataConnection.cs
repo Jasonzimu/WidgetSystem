474543d91f2c5a015155124e3d3a6a7ded0196c0
Zimu Tian <P2617178@my365.dmu.ac.uk>
2021-02-18 01:47:18 +0800

    Add project files.

---
+++ WidgetClasses/clsDataConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    class clsDataConnection
+    {
+        //connection object used to connect to the database
+        SqlConnection connectionToDB = new SqlConnection();
+        //data adapter used to transfer data to and from the database
+        SqlDataAdapter dataChannel = new SqlDataAdapter();
+    }
+     
+}       
